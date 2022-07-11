2ee0aef7b977c967d84cbc0568fc9df712d0a7d0
Leandro Calado Ferreira <83848600+lcaladoferreira@users.noreply.github.com>
2022-07-11 13:15:12 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import MatrixBG from "./MatrixBG";
+
+function App() {
+  return (
+    <div style={{ position: "relative", width: "100vw", height: "100vh" }}>
+      <MatrixBG />
+    </div>
+  );
+}
+
+export default App;
